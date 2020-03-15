8a16ba20c82f4bf350c8c966f9e64e9aca611b71
nikhilgnet <nikhil.gnet@gmail.com>
2020-03-15 22:20:46 +0530

    real world app added

---
+++ real-work-vue/src/views/EventList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Event List</h1>
+    <EventCard />
+    jdjssj
+  </div>
+  
+</template>
+
+<script>
+  import EventCard from '@/components/EventCard.vue';
+  export default {
+    components:{
+      EventCard
+    }
+    
+  }
+</script>
+
+<style lang="scss" scoped></style>
